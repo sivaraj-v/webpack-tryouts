fd1e7b8add63e65abf235a430d11b76fe9cd1fc4
Sivaraj V <sivaraj.v@EF.com>
2018-03-28 20:08:47 +0530

    Notification Checking

--- src/js/app.js
+++ src/js/app.js
@@ -12,74 +12,87 @@ secretButton.addEventListener('click', toggleSecretState);
 updateSecretParagraph();
 
 function toggleSecretState() {
-    showSecret = !showSecret;
-    updateSecretParagraph();
-    updateSecretButton();
+  showSecret = !showSecret;
+  updateSecretParagraph();
+  updateSecretButton();
 }
 
 function updateSecretButton() {
-    if (showSecret) {
-        secretButton.textContent = 'Hide the Secret';
-    } else {
-        secretButton.textContent = 'Show the Secret';
-    }
+  if (showSecret) {
+    secretButton.textContent = 'Hide the Secret';
+  } else {
+    secretButton.textContent = 'Show the Secret';
+  }
 }
 
 function updateSecretParagraph() {
-    if (showSecret) {
-        secretParagraph.style.display = 'block';
-    } else {
-        secretParagraph.style.display = 'none';
-    }
+  if (showSecret) {
+    secretParagraph.style.display = 'block';
+  } else {
+    secretParagraph.style.display = 'none';
+  }
 }
 
-
+var SW_Enabled = false;
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        // navigator.serviceWorker.register('').then(registration => {
-        //     console.log('SW registered: ', registration);
-        //     registration.pushManager.subscribe({ userVisibleOnly: true });
-        // }).catch(registrationError => {
-        //     console.log('SW registration failed: ', registrationError);
-        // });
+  window.addEventListener('load', () => {
+    // navigator.serviceWorker.register('').then(registration => {
+    //     console.log('SW registered: ', registration);
+    //     registration.pushManager.subscribe({ userVisibleOnly: true });
+    // }).catch(registrationError => {
+    //     console.log('SW registration failed: ', registrationError);
+    // });
 
-        navigator.serviceWorker.register('http://localhost:8081/svs-printers-and-bags/dist/sw.js', { scope: 'svs-printers-and-bags/dist/' }).then(function(serviceReg) {
-            debugger
-            if ('pushManager' in serviceReg) {
-                console.log(1);
-            } else {
-                console.log(0);
-            }
+    navigator.serviceWorker.register('http://localhost:8081/svs-printers-and-bags/dist/sw.js', {
+      scope: 'svs-printers-and-bags/dist/'
+    }).then(function(serviceReg) {
+      if ('pushManager' in serviceReg) {
+        console.log(1);
+      } else {
+        console.log(0);
+      }
+      if (serviceReg.installing) {
+        console.log('Service worker installing');
+      } else if (serviceReg.waiting) {
+        console.log('Service worker installed');
+      } else if (serviceReg.active) {
+        SW_Enabled = true;
+        console.log('Service worker active');
+      }
 
-            if (serviceReg.installing) {
-                console.log('Service worker installing');
-            } else if (serviceReg.waiting) {
-                console.log('Service worker installed');
-            } else if (serviceReg.active) {
-                console.log('Service worker active');
-            }
-
-            // Check for showNotification support.
-            if (!(serviceReg.showNotification)) {
-                console.log('Notifications aren\'t supported on service workers.');
-            } else {
-                console.log('Notifications are supported on service workers.');
-
-                serviceWorker.addEventListener("statechange", function(e) {
-                    console.log("sw statechange : ", e.target.state);
-                    if (e.target.state == "activated") {
-                        // use pushManger for subscribing here.
-                        console.log("Just now activated. now we can subscribe for push notification")
-                        serviceReg.pushManager.subscribe({ userVisibleOnly: true });
-                    }
-                });
-            }
-
-        }).catch(function(error) {
-            // registration failed
-            console.log('Registration failed with ' + error);
+      // Check for showNotification support.
+      if (!(serviceReg.showNotification)) {
+        console.log('Notifications aren\'t supported on service workers.');
+      } else {
+        console.log('Notifications are supported on service workers.');
+        serviceWorker.addEventListener("statechange", function(e) {
+          console.log("sw statechange : ", e.target.state);
+          if (e.target.state == "activated") {
+            // use pushManger for subscribing here.
+            console.log("Just now activated. now we can subscribe for push notification")
+          }
         });
+        serviceReg.pushManager.subscribe({
+          userVisibleOnly: true
+        });
+      }
 
+    }).catch(function(error) {
+      // registration failed
+      if (!SW_Enabled) {
+        console.log('Registration failed with ' + error);
+      }
 
     });
+
+    Notification.requestPermission(function(result) {
+      if (result === 'granted') {
+        navigator.serviceWorker.ready.then(function(registration) {
+          registration.showNotification('Notification with ServiceWorker');
+        });
+      }
+    });
+
+
+  });
 }
