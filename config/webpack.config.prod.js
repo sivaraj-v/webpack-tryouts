ed1d2864610cf52c694c48d276f4dc7735fec6d0
Sivaraj V <sivaraj.v@EF.com>
2018-03-30 13:24:34 +0530

    formated

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -8,163 +8,163 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const extractSass = new ExtractTextPlugin({
-    filename: 'css/index-[contenthash].css',
-    disable: process.env.NODE_ENV === 'development',
+  filename: 'css/index-[contenthash].css',
+  disable: process.env.NODE_ENV === 'development',
 });
 let UglifyJsPluginOptions = {
-    uglifyOptions: {
-        compress: true,
-        ie8: true,
-    },
+  uglifyOptions: {
+    compress: true,
+    ie8: true,
+  },
 }
 let cleanOptions = {
-    root: __dirname,
-    exclude: ['notRemove'], //ignore your folder on build
-    verbose: false,
-    dry: false,
-    beforeEmit: false,
-    allowExternal: false,
+  root: __dirname,
+  exclude: ['notRemove'], //ignore your folder on build
+  verbose: false,
+  dry: false,
+  beforeEmit: false,
+  allowExternal: false,
 };
 let htmlWebpackOptions = {
-    template: 'index.ejs',
-    title: 'Progressive Web Application',
-    favicon: 'favicon.ico',
-    meta: [{
-        name: 'robots',
-        content: 'noindex,nofollow'
-    }],
-    minify: {
-        collapseWhitespace: true,
-        conservativeCollapse: true,
-        preserveLineBreaks: false,
-        useShortDoctype: true,
-        html5: true
-    }
+  template: 'index.ejs',
+  title: 'Progressive Web Application',
+  favicon: 'favicon.ico',
+  meta: [{
+    name: 'robots',
+    content: 'noindex,nofollow'
+  }],
+  minify: {
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    preserveLineBreaks: false,
+    useShortDoctype: true,
+    html5: true
+  }
 }
 let WebpackPwaManifestOptions = {
-    name: 'My Progressive Web App',
-    short_name: 'MyPWA',
-    description: 'My awesome Progressive Web App!',
-    display: "standalone",
-    start_url: "/",
-    background_color: '#e60a0a',
-    icons: [{
-            src: path.resolve('src/img/icon.png'),
-            sizes: [16, 32, 57, 60, 72, 76, 96, 114, 120, 128, 144, 152, 180, 192, 256, 384, 512], // multiple sizes
-            destination: path.join('img/icons')
-        },
-        {
-            src: path.resolve('src/img/icon.png'),
-            size: '1024x1024', // you can also use the specifications pattern
-            destination: path.join('img/icons')
-        }
-    ],
-    gcm_sender_id: "103953800507",
+  name: 'My Progressive Web App',
+  short_name: 'MyPWA',
+  description: 'My awesome Progressive Web App!',
+  display: "standalone",
+  start_url: "/",
+  background_color: '#e60a0a',
+  icons: [{
+    src: path.resolve('src/img/icon.png'),
+    sizes: [16, 32, 57, 60, 72, 76, 96, 114, 120, 128, 144, 152, 180, 192, 256, 384, 512], // multiple sizes
+    destination: path.join('img/icons')
+  },
+    {
+      src: path.resolve('src/img/icon.png'),
+      size: '1024x1024', // you can also use the specifications pattern
+      destination: path.join('img/icons')
+    }
+  ],
+  gcm_sender_id: "103953800507",
 }
 let workboxOptions = {
-    globPatterns: [' "**/*.{html,md,css,txt,less,scss,otf,eot,svg,ttf,woff,woff2,jpg,png,gif,yml,js,ico,sh,xml,map}"'],
-    swDest: 'sw.js',
-    swSrc: './src/sw.js',
-    globIgnores: [
-        "**/node_modules/**/*"
-    ]
+  globPatterns: [' "**/*.{html,md,css,txt,less,scss,otf,eot,svg,ttf,woff,woff2,jpg,png,gif,yml,js,ico,sh,xml,map}"'],
+  swDest: 'sw.js',
+  swSrc: './src/sw.js',
+  globIgnores: [
+    "**/node_modules/**/*"
+  ]
 }
 let pathsToClean = [path.resolve(__dirname, 'dist')];
 const swSrc = 'src/sw.js';
 const swDest = 'dist/sw.js';
 module.exports = {
-    entry: './src/js/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle-[chunkhash].js',
-        publicPath: '', // you will get dist configuration virtually else you won't get err: GET http://localhost:8081/dist/bundle.js net::ERR_ABORTED
-    },
-    resolve: {
-        modules: ["node_modules"],
-        extensions: [".js", ".json", ".scss"]
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    enforce: true,
-                    chunks: 'all',
-                    priority: -10,
-                }
-            }
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle-[chunkhash].js',
+    publicPath: '', // you will get dist configuration virtually else you won't get err: GET http://localhost:8081/dist/bundle.js net::ERR_ABORTED
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".json", ".scss"]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          enforce: true,
+          chunks: 'all',
+          priority: -10,
         }
+      }
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [require('@babel/plugin-proposal-object-rest-spread')],
+        },
+      },
     },
-    module: {
-        rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [require('@babel/plugin-proposal-object-rest-spread')],
-                    },
-                },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]',
+            outputPath: 'img',
+          },
+        },],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+        options: {
+          limit: 8192,
+          name: "[name].[ext]?[hash]",
+          emitFile: false
+        }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              minimize: true,
             },
+          },
             {
-                test: /\.(png|jpg|jpeg|gif|svg)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]?[hash]',
-                        outputPath: 'img',
-                    },
-                }, ],
+              loader: 'sass-loader',
             },
-            {
-                test: /\.(woff|woff2|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file-loader",
-                options: {
-                    limit: 8192,
-                    name: "[name].[ext]?[hash]",
-                    emitFile: false
-                }
-            },
-            {
-                test: /\.(s*)css$/,
-                use: extractSass.extract({
-                    use: [{
-                            loader: 'css-loader',
-                            options: {
-                                minimize: true,
-                            },
-                        },
-                        {
-                            loader: 'sass-loader',
-                        },
-                    ],
-                    // use style-loader in development
-                    fallback: 'style-loader',
-                }),
-            },
-            {
-                test: /\.(html)$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true,
-                    },
-                },
-            },
-        ],
-    },
-    plugins: [
-        new UglifyJsPlugin(UglifyJsPluginOptions),
-        extractSass,
-        new HtmlWebpackPlugin(htmlWebpackOptions),
-        new CleanWebpackPlugin(pathsToClean, cleanOptions),
-        new WebpackPwaManifest(WebpackPwaManifestOptions),
-        new WorkboxPlugin.InjectManifest(workboxOptions),
+          ],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            minimize: true,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new UglifyJsPlugin(UglifyJsPluginOptions),
+    extractSass,
+    new HtmlWebpackPlugin(htmlWebpackOptions),
+    new CleanWebpackPlugin(pathsToClean, cleanOptions),
+    new WebpackPwaManifest(WebpackPwaManifestOptions),
+    new WorkboxPlugin.InjectManifest(workboxOptions),
 
 
-    ]
+  ]
 };
 
 // Readme:
